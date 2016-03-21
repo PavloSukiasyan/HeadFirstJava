1a584a896b8be45fb8bb7f640100a5a9d11f33e9
Your Name <pavlo.sukiasyan@gmail.com>
2016-03-21 23:19:25 +0200

    Be The Compiller p. 118 and CodeMagnets p. 119

---
+++ src/headFirstJava/Examples/CodeMagnets/MultiFor.java
@@ -0,0 +1,19 @@
+package headFirstJava.Examples.CodeMagnets;
+
+public class MultiFor {
+
+	public static void main(String[] args) {
+		// page 119
+
+		for (int x = 0; x < 4; x++) {
+			if (x > 1) {
+				x++;
+			}
+
+			for (int y = 4; y > 2; y--) {
+				System.out.println(x + " " + y);
+			}
+		}
+	}
+
+}
